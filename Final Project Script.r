8e8f996c9984eb7c36cade31908bc54d7376a91b
Cecilia Barnard <barnar3@clemson.edu>
2025-06-13 10:46:30 -0400

    Added english stopwords

--- Final Project Script.r
+++ Final Project Script.r
@@ -1,7 +1,7 @@
 Final Project Script #using info from our webscraping script project 
 library(internetarchive)
 library(tidyverse)
-
+library(tidytext)
 Registrumquery <- c("contributor" = "National Library of Scotland", "title" = "Registrum")
 
 ia_search(Registrumquery, num_results = 19) %>% ia_get_items() %>% ia_metadata() %>% filter(field == "title" | field == "external-identifier")
@@ -10,10 +10,20 @@ registrummetadata <- as.data.frame(ia_search(Registrumquery, num_results = 19) %
 
 registrummetadata <- registrummetadata %>% pivot_wider(names_from = field, values_from = value)
 
+
 dir.create("NLSParishRegisterTexts")
 
 ia_search(Registrumquery, num_results = 19) %>% ia_get_items %>% ia_files %>% filter(type == "txt") %>% group_by(id) %>% ia_download(dir = "NLSParishRegisterTexts", overwrite = FALSE,) %>% glimpse()
 library(readtext)
 file_paths <- system.file("NLSParishRegisterTexts/")
 NLStxtfull <- readtext(paste("NLSParishRegisterTexts/", "*.txt", sep = ""))
-NLStxtmeta <- full_join
+NLStxtmeta <- full_join(registrummetadata, NLStxtfull, by = c("id" ="doc_id")) %>% as_tibble()
+write.csv(registrummetadata, "output.csv", row.names =FALSE, quote=FALSE)
+write.csv(NLStxtmeta, "output.csv", row.names=FALSE, quote=FALSE)
+write.csv(NLStxtfull, "output.csv", row.names=FALSE, quote=FALSE)
+
+library("stopwords")
+head(stopwords::data_stopwords_ancient)
+
+tokenized.NLS.full <- NLStxtfull %>% unnest_tokens(word, text)
+tokenized.NLS.full <- tokenized.NLS.full %>% anti_join(stop_words)
