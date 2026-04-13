089fd7516e29697284d5d42a8872f4a6afacc064
Cecilia Barnard <barnar3@clemson.edu>
2026-04-13 15:46:56 -0400

    added as_tibble to line 21 per copilot to fix a miscommunication between a readtext file and a dataframe

--- Final Project Script.r
+++ Final Project Script.r
@@ -18,13 +18,13 @@ head(stopwords::data_stopwords_ancient)
 latinstopwords <- as_tibble(stopwords(language = "la", source = "ancient", simplify = TRUE))
 
 
-tokenized.MT.full <- monastictexts %>% unnest_tokens(word, text)
+tokenized.MT.full <- monastictexts %>% unnest_tokens(word, text) %>% as_tibble()
 tokenized.MT.full <- tokenized.MT.full %>% anti_join(stop_words)
 tokenized.MT.full.stopwords <- tokenized.MT.full %>% anti_join(stopwords(language = "la", source = "ancient"))
 
 latinstopwords <- as.data.frame(stopwords(language = "la", source = "ancient"))
 
-tokenized.MT.full.stopwords <- tokenized.MT.full %>% anti_join(latinstopwords, tokenized.MT.full, join_by(stopwords(language = "la", source = "ancient", by = join_by(word))))
+tokenized.MT.full.stopwords <- tokenized.MT.full %>% anti_join(latinstopwords, tokenized.MT.full, join_by(stopwords(language = "la", source = "ancient")))
 ??anti_join
 
 install.packages("dplyr")
