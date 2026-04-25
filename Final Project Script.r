3b5c1c34c016c2b6b54b60736f811866dfec2096
Cecilia Barnard <barnar3@clemson.edu>
2026-04-24 20:07:10 -0400

    added custom stopwords

--- Final Project Script.r
+++ Final Project Script.r
@@ -8,16 +8,15 @@ library(stopwords)
 library(quanteda)
 ??stopwords
 head(stopwords::data_stopwords_ancient)
-
+customstopwords <- as_tibble("customstopwords.csv")
 latinstopwords <- tibble(word = stopwords(language = "la", source = "ancient", simplify = TRUE))
-
 tokenized.MT.full <- monastictexts %>% unnest_tokens(word, text) %>% as_tibble()
 tokenized.MT.full <- tokenized.MT.full %>% anti_join(stop_words)
 tokenized.MT.full.stopwords <- tokenized.MT.full %>% anti_join(latinstopwords)
 
 library(dplyr)
 # Combine English and Latin stopwords, making sure both have a 'word' column
-englishstopwords <- stop_words %>% select(word)
+englishstopwords <- (stop_words) %>% select(word)
 completestopwords <- bind_rows(englishstopwords, latinstopwords) %>% distinct()
 tokenized.MT.full.stopwords <- tokenized.MT.full %>% anti_join(completestopwords)
 
@@ -35,4 +34,17 @@ tokenizedMH %>% count(word, sort = TRUE) %>% filter(n > 350) %>% ggplot(aes(x = 
 
 #Topic Models
 
-
+#Latin
+library(tm)
+library(topicmodels)
+install.packages("reshape2")
+library(reshape2)
+tidy.MT <- tokenized.MT.full.stopwords %>% filter('ocr', 'png', 'results') %>% filter(str_detect(word, "[a-z]$")) %>% count(doc_id, word)
+MT.dtm <- tidy.MT %>% count(doc_id, word) %>% cast_dtm(doc_id, word, n)
+MT.lda <- LDA(MT.dtm, k = 35, control = list(seed = 12345))
+MT.lda
+MT.topics <- tidy(MT.lda, matrix = "beta")
+head(MT.topics)
+topterms.MT <- MT.topics %>% arrange(desc(beta)) %>% group_by(topic) %>% slice(1:10)
+topterms.MT %>% mutate(term = reorder_within(term, beta, topic)) %>%
+ggplot(aes(beta, term, fill = factor(topic))) + geom_col(show.legend = FALSE) + facet_wrap(~ topic, scales = "free") + scale_y_reordered()
